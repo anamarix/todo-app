d6f772aaa78542fb4ca823ab9eec28bef11e5bcd
anamarix <annamariakomorowicz@gmail.com>
2022-01-17 18:10:05 +0100

    Updates

---
+++ precache-manifest.cee2d60435158437da4a6627201628e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81359e2f9c51532cc70c94606489775",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "707a58cec2ff5958867d",
+    "url": "/todo-app/static/css/main.acd1ef68.chunk.css"
+  },
+  {
+    "revision": "9141d66e283647d104c8",
+    "url": "/todo-app/static/js/2.03258504.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todo-app/static/js/2.03258504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707a58cec2ff5958867d",
+    "url": "/todo-app/static/js/main.df1683ae.chunk.js"
+  },
+  {
+    "revision": "370cd59a464de2a43c23",
+    "url": "/todo-app/static/js/runtime-main.ce1d5fea.js"
+  }
+]);
